aee8b605b039bdc83d5272f1dbf805f92a0f29e1
Pavel Kurnosov <pavel.kurnosov2016@yandex.com>
2017-01-09 10:07:55 +0200

    initialization

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('flysat')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
